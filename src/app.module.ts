9b2483dc60447420e0ff96fe76dde18dbf744a65
ho3eintahan <hoseintahan84@gmail.com>
2025-03-23 22:32:11 +0330

    Update    app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookEntity } from 'src/book/entity/book.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { BookResolver } from './book/book.resolver';
+import { BookModule } from './book/book.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'library',
+        entities: [BookEntity],
+        synchronize: true,
+      }
+    ),
+    BookModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BookResolver],
 })
-export class AppModule {}
+export class AppModule { }
